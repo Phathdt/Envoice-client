b60f039a1fbf5d6044a494f79a61c0975aebe325
Phathdt <phathdt.leviateam@gmail.com>
2018-12-01 00:45:24 +0700

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
 admin = User.create(email: 'owner@envoice.com', password: '123456', name: 'Owner', role: 1)
 
-Company.new.save(validate: false)
+Company.create(
+  name: "MILO ",
+  legal_representative: "Khoa Nguyen",
+  phone_number: "01647778186",
+  tax_identification_number: "91231283",
+  address: "Quan 2 Ho Chi Minh",
+  email: "khoa@niviki.com",
+)
