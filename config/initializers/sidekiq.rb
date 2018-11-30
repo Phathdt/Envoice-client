ca4e7722b9c7365f3c860149074d517ff31a7437
Phathdt <phathdt.leviateam@gmail.com>
2018-12-01 00:54:37 +0700

    update sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,7 @@
-REDIS_HOST = ENV['redis_host'] || '127.0.0.1'
-SIKEKIQ_REDIS_URL = "redis://#{REDIS_HOST}:6379/0".freeze
-
-Sidekiq.configure_server do |config|
-  config.redis = {
-    url: SIKEKIQ_REDIS_URL
-  }
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV['REDIS_URL'] }
 end
 
-Sidekiq.configure_client do |config|
-  config.redis = {
-    url: SIKEKIQ_REDIS_URL
-  }
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV['REDIS_URL'] }
 end
