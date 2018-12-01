550b0bd5a0c1f4d572021a7701a9fbf39eed74c3
Phathdt <phathdt.leviateam@gmail.com>
2018-12-01 18:22:41 +0700

    update sync data job

--- app/jobs/sync_data_job.rb
+++ app/jobs/sync_data_job.rb
@@ -4,12 +4,24 @@ class SyncDataJob < ApplicationJob
   def perform(invoice)
     invoice.reload
 
-    payload = {
-      invoice: invoice.slice(:form_identifier, :serial_number, :invoice_number,:invoice_date, :payment_method, :vat_percent, :transaction_id, :hash_data).merge(
-        pdf: File.open(ActiveStorage::Blob.service.send(:path_for, invoice.pdf.key)),
-        image: File.open(ActiveStorage::Blob.service.send(:path_for, invoice.image.key))
-      )
-    }
+    payload =
+      if Rails.env.production?
+        image_path = RestClient::Request.execute(method: :get, url: invoice.image.service_url, raw_response: true).file.path
+        pdf_path = RestClient::Request.execute(method: :get, url: invoice.pdf.service_url, raw_response: true).file.path
+        {
+          invoice: invoice.slice(:form_identifier, :serial_number, :invoice_number,:invoice_date, :payment_method, :vat_percent, :transaction_id, :hash_data).merge(
+            pdf: File.open(pdf_path),
+            image: File.open(image_path)
+          )
+        }
+      else
+        {
+          invoice: invoice.slice(:form_identifier, :serial_number, :invoice_number,:invoice_date, :payment_method, :vat_percent, :transaction_id, :hash_data).merge(
+            pdf: File.open(ActiveStorage::Blob.service.send(:path_for, invoice.pdf.key)),
+            image: File.open(ActiveStorage::Blob.service.send(:path_for, invoice.image.key))
+          )
+        }
+      end
 
     header = {
       content_type: :json,
