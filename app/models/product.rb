f0cc8dc0514a54095ca647812a80f3eeb1ccfbb0
Phathdt <phathdt.leviateam@gmail.com>
2019-02-07 19:20:46 +0700

    add validate product must have price greater than zero

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,5 @@ class Product < ApplicationRecord
 
   validates :name, presence: true
   validates :unit, presence: true
-  validates :price, presence: true, numericality: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
